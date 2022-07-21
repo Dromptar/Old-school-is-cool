be929fefd1f86bb75aa17f4b5385012c74b33509
Simon <simon.morvant@yahoo.com>
2022-07-21 10:17:47 -0300

    Creating new project

---
+++ App.js
@@ -0,0 +1,7 @@
+function menuToggle() {
+    let nav = document.getElementById('nav');
+    let toggle = document.getElementById('toggle');
+    nav.classList.toggle("active");
+    toggle.classList.toggle("active");
+
+}
